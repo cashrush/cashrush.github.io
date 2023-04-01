e94d8e55a625b072e0059b116a08abe9d6b7e951
an-ivannikov <info@ivannikov.pro>
2023-04-01 12:16:38 +0400

    Updates

---
+++ static/js/5.49f9b9a1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@cashrush/cashrush-react"]=this["webpackJsonp@cashrush/cashrush-react"]||[]).push([[5],{433:function(s,h){}}]);
+//# sourceMappingURL=5.49f9b9a1.chunk.js.map
--- static/js/5.59024bd0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@cashrush/cashrush-react"]=this["webpackJsonp@cashrush/cashrush-react"]||[]).push([[5],{511:function(s,h){}}]);
-//# sourceMappingURL=5.59024bd0.chunk.js.map
